a920165c2dc3d6a371aeb5bedba43de05a439fe4
cameronlevine <cameronlevine95@gmail.com>
2017-03-02 21:05:21 -0500

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://goo.gl/forms/Akc2mpeOEJF3s1Ji1';
+$url = 'https://docs.google.com/a/buffalo.edu/forms/d/1bAcsOj8JPEWUI4gM9wnp7KsZ6-4vRmAdC5_f9VIxJjY/edit';
 
 header('Location: '.$url);
