aa5c6e0ee1af358533f69abeb558bde246fc321d
cameronlevine <cameronlevine95@gmail.com>
2017-03-09 19:54:40 -0500

    attendance
    attendance

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $open = false;
-$url = 'https://docs.google.com/a/buffalo.edu/forms/d/e/1FAIpQLSdUeFQBYuSwSDa2KWj6Sp1ziZXo0t2eb5bw2ZLc1zkME34fjw/viewform';
+$url = 'https://goo.gl/forms/18WBcE6fuuBmgByw2';
 
 if ( !$open ) {
     $url = 'https://ubnetdef.org';
