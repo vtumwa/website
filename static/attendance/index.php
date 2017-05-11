322be104e7e8781f8463272194816f8dc697ef2c
James Droste <james@droste.im>
2017-05-11 19:26:48 -0400

    thnks fr th mmrs

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://goo.gl/forms/wWOlrYzT1yHe7B9t2';
+$url = 'https://docs.google.com/a/buffalo.edu/forms/d/e/1FAIpQLSeScoiPmmGjhwXou5GSY5uDiVyzJQ7knJCnrpPsIP99sgB3fg/viewform?usp=sf_link';
 
 header('Location: ' . $url);
