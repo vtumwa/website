1be97f9a24a2a4ff9611f574ad3ad421f3adf0fd
canadared3 <canadared3@gmail.com>
2017-09-07 18:44:50 -0400

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://goo.gl/forms/1k2G9RQbATAVRHpH2';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSdb42HQBXjjQyclSxBN8awF4SsZ8DkOjI8gvEYY0kE7m1clZA/viewform?usp=sf_link';
 
 header('Location: ' . $url);
