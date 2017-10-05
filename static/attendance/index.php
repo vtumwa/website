247e420bee5235497a6ea27d448d58d06ea7b797
canadared3 <canadared3@gmail.com>
2017-10-05 18:45:38 -0400

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfV19zc4hP6BQ-HlSZTsZm4HO0rdtkhh8h6mSuU5a6Xn4vAYA/viewform?usp=sf_link';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSc6oLYA1DEWAtTlhn57ByyhMyNBBV4Gg_ugj95qGcHD0dbXiw/viewform?usp=sf_link';
 
 header('Location: ' . $url);
