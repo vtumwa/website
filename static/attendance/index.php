24c770892dfd2e7b972f3fcb9beab10638975293
rossventresca <rossvent@buffalo.edu>
2017-10-12 20:43:04 -0400

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSc6oLYA1DEWAtTlhn57ByyhMyNBBV4Gg_ugj95qGcHD0dbXiw/viewform?usp=sf_link';
+$url = 'https://goo.gl/forms/zU9ELhBoXsAiiHIr1';
 
 header('Location: ' . $url);
