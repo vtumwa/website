4723ba2e1461016b6c3202d5fb554fda44874b2b
Prasanna166 <32180952+Prasanna166@users.noreply.github.com>
2017-10-19 18:42:33 -0400

    Update index.php

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://goo.gl/forms/zU9ELhBoXsAiiHIr1';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfccG3VJY3rOpu2eluZd70coTvKEPsp3hllZ3JQLUNxQtdFlA/viewform?usp=sf_link';
 
 header('Location: ' . $url);
