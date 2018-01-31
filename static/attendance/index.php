28b47d5aeed478ebaa97f92ed113a98a83e82db0
Alexander Bitar <abitar@buffalo.edu>
2018-01-31 14:48:31 -0500

    Updated the Attendance for 2/1/2018

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfQ25U39HjUU2IPAyyuOCgDfjFMu2xki91Bv5_02CbaBbrm4A/viewform?usp=sf_link';
+$url = 'https://goo.gl/forms/VSbgb6HYY3UQg6W32';
 
 header('Location: ' . $url);
