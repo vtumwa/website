b28785b6b0d29297b162cd673fdea74f79da6007
AlexBitar <abitar@buffalo.edu>
2018-03-29 21:10:38 -0400

    Updated Attendance 3-29-18

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLScyxKI8NTjQOtmBzQAWhtqlpCEOTK2lAFR25BAEzt-z96vzqw/viewform';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSePkRp8V_OZXdZMV7v5QKTJCAmcQiMAAJWbdoudcVdIhgoxjA/viewform?usp=sf_link';
 
 header('Location: ' . $url);
