15c28403d9ab72367362cdbefb492cb10170aa13
Stephen James <stephenorjames@gmail.com>
2019-02-07 13:16:52 -0500

    Update attendance for for 2019-02-07

--- static/attendance/index.php
+++ static/attendance/index.php
@@ -1,4 +1,4 @@
 <?php
-$url = 'https://goo.gl/forms/Gkg1YmOxnWvzSOka2';
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLSd_Ok6SdVbE280WvCyWDkPd6315bzpg2z3sJ76zQQM39bEIZQ/viewform';
 
 header('Location: ' . $url);
